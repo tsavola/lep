07c6548c8c95d7ce4e6a5e82fe5da5334308de81
Timo Savola <timo.savola@iki.fi>
2021-06-12 08:27:43 +0300

    MIT license

--- src/display.rs
+++ src/display.rs
@@ -1,5 +1,5 @@
-// Copyright (c) 2019 Timo Savola. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright (c) 2019 Timo Savola.
+// Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
 use super::eval::Ref;
